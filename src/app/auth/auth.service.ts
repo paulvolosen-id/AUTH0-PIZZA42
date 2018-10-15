6e0bb314c8f92cc3e938050ef8e799c01268b390
Paul Volosen <pvolosen@identropy.com>
2018-10-15 00:01:50 -0500

    updated scopes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AUTH_CONFIG } from './auth0-variables';
 import { Router } from '@angular/router';
-import 'rxjs/add/operator/filter';
+// import 'rxjs/add/operator/filter';
+import { filter } from 'rxjs/operators';
 import * as auth0 from 'auth0-js';
 
 (window as any).global = window;
@@ -15,7 +16,7 @@ export class AuthService {
     responseType: 'token id_token',
     audience: AUTH_CONFIG.apiUrl,
     redirectUri: AUTH_CONFIG.callbackURL,
-    scope: 'openid profile email gender read:messages'
+    scope: 'openid profile read:email read:email_verified read:gender read:messages'
   });
 
   userProfile: any;
@@ -23,17 +24,12 @@ export class AuthService {
   constructor(public router: Router) {}
 
   public login(): void {
-    //console.log('--> isAuth in login: ' + this.isAuthenticated() );
     this.auth0.authorize();
   }
 
   public handleAuthentication(): void {
-  //  console.log ('ENTER: handleAuthentication()');
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('HANDLEAUTHENTICATION [authResult stringiFied]: ' + JSON.stringify(authResult));
-     //  console.log('authResult JSON.PARSE: ' + JSON.parse(authResult));
-
         this.setSession(authResult);
         this.setEmailVerificationStatus(authResult); // paulvo: setEmailVerificationStatus
         this.router.navigate(['/home']);
@@ -43,7 +39,6 @@ export class AuthService {
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
     });
-  //  console.log ('EXIT: handleAuthentication()');
   }
 
   public getProfile(cb): void {
@@ -70,7 +65,6 @@ export class AuthService {
   }
 
   public logout(): void {
-  //  console.log('ENTER: logout() ');
 
     // Remove tokens and expiry time from localStorage
     localStorage.removeItem('access_token');
@@ -80,27 +74,17 @@ export class AuthService {
 
     localStorage.clear(); // ADDED: Paulvo
     // Go back to the home route
-  //  console.log('-->> isAuth in logout: ' + this.isAuthenticated() );
-    console.log('EXIT: logout() ');
-
     this.router.navigate(['/']); // paulvo: added home
   }
 
   public isAuthenticated(): boolean {
     // Check whether the current time is past the
     // access token's expiry time
- //   console.log('ENTER: isAuthenticated() ');
     const expiresAt = JSON.parse(localStorage.getItem('expires_at') || '{}');
- //   console.log('-->expireAt from localStorage prior to check: ' + expiresAt);
     let isAuth = false;
     isAuth = new Date().getTime() < expiresAt;
-  //  console.log(' --->isAuth post check: ' + isAuth);
-    // alert('isAuth: ' + isAuth);
-  //  console.log('EXIT: isAuthenticated()');
     return isAuth;
     // return (new Date().getTime() < expiresAt);
-
-
   }
 
   public isEmailVerified() : boolean { // TODO: we should check this on the auth profile
@@ -111,48 +95,15 @@ export class AuthService {
       localStorage.setItem('emailVerified', 'true');
   }
 
-  /* public returnAuthResult(): Object {
-    console.log ('ENTER: returnAuthResult()');
-    this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('authResult: ' + authResult);
-        return authResult;
-      //  this.setSession(authResult);
-    //    this.router.navigate(['/home']);
-      } else if (err) {
-       // this.router.navigate(['/home']);
-        console.log(err.error);
-        alert(`Error: ${err.error}. Check the console for further details.`);
-      }
-    });
-    console.log ('EXIT: returnAuthResult()');
-  } */
 
-  /*public updateUserProfileEmailVerificationStatus2(authResult) : void { // should set email verified on user profile?
-    this.auth0.client.userInfo(authResult.accessToken, function(err, user) {
-      if (err) {
-        console.log('ERROR getting userInfo: ' + err);
-        alert('ERROR getting userInfo: ' + err);
-       // return false;
-      } else {
-        console.log ('SUCCESS: get email verification status on userprofile..' + JSON.parse(user.email_verified));
-      // localStorage.setItem('emailVerified', user.email_verified);
-        user.email_verified = true;
-      }
-    });
-} */
 
   public setEmailVerificationStatus(authResult) : void  {
     // auth0.client.userInfo(authResult.accessToken, function(err, user) {
       this.auth0.client.userInfo(authResult.accessToken, function(err, user) {
       if (err) {
-     //   console.log('ERROR getting userInfo: ' + err);
         alert('ERROR getting userInfo: ' + err);
-       // return false;
       } else {
-        console.log ('SUCCESS: get email verification status on userprofile..' + JSON.parse(user.email_verified));
         if (user.email_verified) { // paulvo: added. Check we can read
- //         console.log ('--> user.email_verified' + user.email_verified);
           localStorage.setItem('emailVerified', user.email_verified);
         } else {
           localStorage.setItem('emailVerified', 'false');
